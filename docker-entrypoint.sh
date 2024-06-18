a968d857f9e2b410759dc9ac6416d971cf0a6000
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-06-18 17:44:49 +0530

    chore: disable auth for debug issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,10 +2,10 @@
 set -e
 
 # raise error if AUTH_TOKEN is not set
-if [ -z "$AUTH_TOKEN" ]; then
-	echo "ERROR: AUTH_TOKEN is not set"
-	exit 1
-fi
+# if [ -z "$AUTH_TOKEN" ]; then
+# 	echo "ERROR: AUTH_TOKEN is not set"
+# 	exit 1
+# fi
 
 # replace ${AUTH_TOKEN} with the value of AUTH_TOKEN
 sed "s/\${AUTH_TOKEN}/$AUTH_TOKEN/g" /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
