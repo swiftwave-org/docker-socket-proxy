a1bb09b72b0d0fc507fff20e7ca82be785d110ea
Tanmoy Sarkar <57363826+tanmoysrt@users.noreply.github.com>
2024-06-19 10:38:56 +0530

    feat: remove authentication

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
 set -e
 
-# raise error if AUTH_TOKEN is not set
-if [ -z "$AUTH_TOKEN" ]; then
-	echo "ERROR: AUTH_TOKEN is not set"
-	exit 1
-fi
-
-# replace ${AUTH_TOKEN} with the value of AUTH_TOKEN
-sed "s/\${AUTH_TOKEN}/$AUTH_TOKEN/g" /usr/local/etc/haproxy/haproxy.cfg.template > /usr/local/etc/haproxy/haproxy.cfg
+cp /usr/local/etc/haproxy/haproxy.cfg.template /usr/local/etc/haproxy/haproxy.cfg
 
 # start haproxy
 if [ "${1#-}" != "$1" ]; then
